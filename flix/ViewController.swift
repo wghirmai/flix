c06e6f904d0813ce7c62482a7c0a9c0e8ebe1ae8
Winta Ghirmai <wghirmai@middlebury.edu>
2020-09-24 15:21:50 -0400

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Winta Ghirmai on 9/24/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
